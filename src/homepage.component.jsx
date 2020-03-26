1f1b0ee62b3e297f440a81d622610cc21260f07f
cansub1974 <jasonalexandermckay@hotmail.com>
2020-03-26 10:50:22 -0400

    Testing remote GitHub

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -7,7 +7,7 @@ function Homepage() {
             <div className='directory-menu'>
                 <div className='menu-item'>
                     <div className='content'>
-                        <h1 className='title'>HATS OFF</h1>
+                        <h1 className='title'>HATS</h1>
                         <span className='subtitle'>SHOP NOW</span>
                     </div>
                 </div>
