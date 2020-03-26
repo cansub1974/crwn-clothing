67a45ed4e54d6d1624dc8dc5f466177bf4a797e3
cansub1974 <jasonalexandermckay@hotmail.com>
2020-03-26 13:18:35 -0400

    Broken Homepage into smaller components.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.scss'
+import '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component';
+
+
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>    
+    )   
+}
+
+export default Homepage;
